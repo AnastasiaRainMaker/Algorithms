fa0657585f7702f911b3b5dcf3c71490fb8e5ffa
Anastasia <humaine@yandex.ru>
2017-12-23 16:31:31 +0300

    urlify algo

---
+++ URLify/src/com/rainmaker/Main.java
@@ -0,0 +1,27 @@
+package com.rainmaker;
+
+public class Main {
+
+    public static void main(String[] arg){
+        URLify("We are going out");
+    }
+
+    private static void URLify(String s) {
+        // quick solution
+        String nS = s.replace(" ", "%20");
+
+        //more complex solution
+        
+//        char[] sChars = s.toCharArray();
+//        StringBuilder nS = new StringBuilder();
+//        char spaceCh = " ".charAt(0);
+//	    for (int i = 0; i < s.length(); i++) {
+//	        if (sChars[i] == spaceCh ) {
+//	            nS.append("%20");
+//            } else  {
+//	            nS.append(sChars[i]);
+//            }
+//        }
+        System.out.print(nS);
+    }
+}
